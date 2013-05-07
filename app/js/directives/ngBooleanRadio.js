f1c955d18b0526a222cdc9e4f11ddc18f7364684
Michael Moussa <michael.moussa@gmail.com>
2013-05-06 22:58:39 -0400

    true / false => "true" / "false"

--- app/js/directives/ngBooleanRadio.js
+++ app/js/directives/ngBooleanRadio.js
@@ -2,5 +2,28 @@ var ngBooleanRadio = function () {
     "use strict";
 
     return {
+        require: "ngModel",
+        link:    function (scope, element, attrs, ngModelController) {
+            /**
+             * Map boolean true / false to string "true" / "false", respectively.
+             *
+             * @return {string} "true" if modelValue is true, "false" if modelValue
+             *                  is false, undefined otherwise.
+             */
+            ngModelController.$formatters.push(function (modelValue) {
+                if (modelValue === true)
+                {
+                    return "true";
+                }
+                else if (modelValue === false)
+                {
+                    return "false";
+                }
+                else
+                {
+                    return undefined;
+                }
+            });
+        }
     };
 };
--- test/directives/ngBooleanRadio_spec.js
+++ test/directives/ngBooleanRadio_spec.js
@@ -9,7 +9,30 @@ describe("ngBooleanRadio", function () {
         $rootScope = $rs;
     }]));
 
-    it("should not do anything useful yet", function () {
-        expect(true).toBe(true);
+    it("should set the view value to string 'true' when the model value is set to boolean 'true'", function () {
+        var element = $compile('<input ng-model="myModel" type="radio" ng-boolean-radio />')($rootScope);
+
+        $rootScope.myModel = true;
+        $rootScope.$apply();
+
+        expect(element.controller("ngModel").$viewValue).toBe("true");
+    });
+
+    it("should set the view value to string 'false' when the model value is set to boolean 'false'", function () {
+        var element = $compile('<input ng-model="myModel" type="radio" ng-boolean-radio />')($rootScope);
+
+        $rootScope.myModel = false;
+        $rootScope.$apply();
+
+        expect(element.controller("ngModel").$viewValue).toBe("false");
+    });
+
+    it("should set the view value to 'undefined' when the model value is neither true nor false", function () {
+        var element = $compile('<input ng-model="myModel" type="radio" ng-boolean-radio />')($rootScope);
+
+        $rootScope.myModel = "hello, world!";
+        $rootScope.$apply();
+
+        expect(element.controller("ngModel").$viewValue).toBeUndefined();
     });
 });
